0a2abdcf17d670b1898034954d720e780b0b6165
tegargnwn <tegarheidar25@gmail.com>
2023-05-18 22:29:56 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ListKontakPage(),
     );
   }
 }
